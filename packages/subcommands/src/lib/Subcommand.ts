bb1d1fdbcb075a67fcb98ed6975a943fb72838a2
c43721 <rlahman.game@gmail.com>
2022-04-09 16:36:19 -0500

    feat: push

--- packages/subcommands/src/lib/Subcommand.ts
+++ packages/subcommands/src/lib/Subcommand.ts
@@ -3,7 +3,7 @@ import type { CommandInteraction, Message } from 'discord.js';
 import { ChatInputSubcommandGroupMappings, ChatInputSubcommandMappings, type SubcommandMappingsArray } from './SubcommandMappings';
 
 export class SubCommand extends Command {
-	public readonly subCommands = null;
+	public readonly subCommands: Map<string, SubcommandMappingsArray>;
 
 	public constructor(context: PieceContext, options: SubCommandPluginCommandOptions) {
 		super(context, options);
@@ -12,8 +12,17 @@ export class SubCommand extends Command {
 	}
 
 	public messageRun(message: Message, args: Args, context: CommandContext): Awaitable<unknown> {
-		// TODO: Implement current behavior (pickResult + match on result)
-		throw new Error('unimplemented');
+		// Pick one argument, then try to match a subcommand:
+		args.save();
+		const value = args.nextMaybe();
+
+		if (value.exists && this.subCommands.has(value.value)) {
+			// TODO: ? ?
+		}
+
+		// No subcommand matched, let's restore and try to run default, if any:
+		args.restore();
+		throw new Error(`The command ${this.name} does not support sub-commands.`);
 	}
 
 	public chatInputRun(interaction: CommandInteraction): Awaitable<unknown> {
@@ -23,7 +32,7 @@ export class SubCommand extends Command {
 		const group = interaction.options.getSubcommandGroup();
 
 		if (!subcommand && !group) {
-			// TODO: figure out what to do when no subcommands nor groups are found
+			throw new Error(`The command ${this.name} does not support sub-commands.`);
 		}
 
 		if (this.subCommands.has(subcommand) || this.subCommands.has(group)) {
@@ -31,11 +40,21 @@ export class SubCommand extends Command {
 
 			if (mappedSubcommand instanceof ChatInputSubcommandMappings) {
 				// TODO: We have chat input subcommand, run it and check result
+				const toRun = mappedSubcommand.subcommands.find((scmd) => scmd.name === subcommand);
+
+				if (toRun) {
+					try {
+					} catch {}
+				}
 			} else if (mappedSubcommand instanceof ChatInputSubcommandGroupMappings) {
 				// TODO: We have chat input group, run it and check result
 			}
 		}
 	}
+
+	#handleChatinput() {}
+
+	#handleMessageRun() {}
 }
 
 export interface SubCommandPluginCommandOptions<ArgType extends Args = Args, CommandType extends Command<ArgType> = Command<ArgType>>
